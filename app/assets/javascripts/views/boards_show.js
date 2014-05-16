03097278a90785b714a7e8421b164a322d77264b
George Groh <george.groh@gmail.com>
2014-05-16 16:39:42 -0700

    Add list new form to Board show page

--- app/assets/javascripts/views/boards_show.js
+++ app/assets/javascripts/views/boards_show.js
@@ -1,8 +1,14 @@
 window.Trellino.Views.BoardShow = Backbone.View.extend({
 	template: JST['boards/show'],
+	
 	initialize: function () {
 		this.listenTo(this.model, "sync", this.render)
 	},
+	
+	events: {
+		"click #destroy":"removeBoard"
+	},
+	
 	render: function () {
 		var view = this;
 		var content = this.template({
@@ -10,7 +16,18 @@ window.Trellino.Views.BoardShow = Backbone.View.extend({
 		});
 		
 		this.$el.html(content);
+		var formView = new Trellino.Views.NewList();
+		this.$("#new-list-form").html(formView.render().$el);
 		
 		return this;
 	},
+	
+	removeBoard: function (event) {
+		event.preventDefault()
+		this.model.destroy();
+		Backbone.history.navigate("#", {trigger: true});
+	},
+	
+	//render new list
+	
 })
--- app/assets/javascripts/views/lists_new.js
+++ app/assets/javascripts/views/lists_new.js
@@ -1,4 +1,25 @@
-window.Trellino.Views.NewList = Backbone.Views.extend({
+window.Trellino.Views.NewList = Backbone.View.extend({
 	template: JST['lists/new'],
+	events: {
+		"submit form":"submit",
+	},
 	
+	render: function() {
+		var content = this.template({ list: this.model })
+		this.$el.html(content);
+		return this;
+	},
+	
+	submit: function(event) {
+		event.preventDefault();
+		var listData = $(event.currentTarget).serializeJSON();
+		var newList = new Trellino.Models.List(listData);
+		
+		newList.save({}, {
+			success: function () {
+				view.model.lists().add(list);
+				view.render()
+			}
+		});
+	},
 });
